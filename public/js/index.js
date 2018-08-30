b68750720247574678402f2aabae0433d3b87999
Dan McKinney <3dhdaniel@gmail.com>
2018-08-30 09:32:55 -0400

    sockets

--- public/js/index.js
+++ public/js/index.js
@@ -2,10 +2,7 @@ var socket = io();
 
 socket.on('connect', () => {
     console.log('Connected to server')
-    socket.emit('createMessage', {
-        from: 'Daniel',
-        text: 'Still looking'
-    })
+    
 });
 
 socket.on('disconnect', () => {
